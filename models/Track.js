321e5b177ad7897e0dfad3b9e11b3986a981abbd
Adophilus <uchenna19of@gmail.com>
2022-10-21 17:40:56 +0100

    adjusted the structure of the Track model

--- models/Track.js
+++ models/Track.js
@@ -1,6 +1,10 @@
 import mongoose from 'mongoose'
 
 export default (mongoose.models.Track ?? mongoose.model('Track', mongoose.Schema({
+  slug: {
+    type: String,
+    required: true
+  },
   name: {
     type: String,
     required: true
