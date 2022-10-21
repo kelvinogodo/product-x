4d51b78997613b8b92193fb674fc1301be2f9d80
Adophilus <uchenna19of@gmail.com>
2022-10-21 17:41:09 +0100

    modded the db.js file

--- utils/db.js
+++ utils/db.js
@@ -3,4 +3,9 @@ import * as dotenv from 'dotenv'
 
 dotenv.config()
 
-mongoose.connect(process.env.MONGO_URI, { newUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI)
+const db = mongoose.connection
+
+db.on('error', (e) => {
+  console.log('database connection error', e)
+})
