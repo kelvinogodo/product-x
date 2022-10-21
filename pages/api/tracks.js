adae88de7d5aa9f347f8b4cc56667815b3065635
Adophilus <uchenna19of@gmail.com>
2022-10-21 17:42:04 +0100

    modded the existing tracks endpoint

--- pages/api/tracks.js
+++ pages/api/tracks.js
@@ -1,12 +1,21 @@
-import db from '@/utils/db'
 import Track from '@/models/Track'
 import {ReasonPhrases, StatusCodes} from 'http-status-codes'
 
 export default async function handler (req, res) {
   switch (req.method) {
     case 'GET':
-      return res.status(ReasonPhrases.OK).send(await Track.find())
+      return res.status(StatusCodes.OK).send(await Track.find())
+    case 'PUT':
+      const trackName = req.body.name
+      try {
+        const track = await Track.create({ name: trackName })
+        return res.status(StatusCodes.CREATED).send(track)
+      }
+      catch (err) {
+        console.log(err)
+        return res.send(StatusCodes.INTERNAL_SERVER_ERROR).send({ error: ReasonPhrases.INTERNAL_SERVER_ERROR })
+      }
     default:
-      return res.status(StatusCodes.METHOD_NOT_ALLOWED).send(ReasonPhrases.METHOD_NOT_ALLOWED)
+      return res.status(StatusCodes.METHOD_NOT_ALLOWED).send({ error: ReasonPhrases.METHOD_NOT_ALLOWED })
   }
 }
